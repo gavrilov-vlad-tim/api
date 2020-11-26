b8ee48c7ccde2b7aa32490842cbb5ad4abfc52f9
Vladimir <gavrilov.vlad.tim@gmail.com>
2020-11-26 17:10:08 +0300

    Edit admin api

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Follow, Group
+from .models import Follow, Group, Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['text', 'pub_date', 'author', 'group']
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Follow)
 admin.site.register(Group)
